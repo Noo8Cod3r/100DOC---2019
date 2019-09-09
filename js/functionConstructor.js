e90e906e9c15d8dd56418fa897ea46f1099548ab
Noo8 Cod3r <noo8cod3r@gmail.com>
2019-09-09 01:53:24 -0500

    Constructor Function

--- js/functionConstructor.js
+++ js/functionConstructor.js
@@ -5,11 +5,18 @@ let Person = function(name, yearOfBirth, job) {
   this.name = name;
   this.yearOfBirth = yearOfBirth;
   this.job = job;
-  this.calculateAge = () => {
-    console.log(2019 - this.yearOfBirth)
-  }
+}
+
+Person.prototype.calculateAge = function() {
+  console.log(2019 - this.yearOfBirth)
 }
 
 let john = new Person('John', 1990, 'Teacher')
+let jane = new Person('jane', 1934, 'Retired')
+let mark = new Person('Mark', 1990, 'Designer')
+let smith = new Person('Smith', 1990, 'Garbage Man')
 
 john.calculateAge()
+jane.calculateAge()
+mark.calculateAge()
+smith.calculateAge()
